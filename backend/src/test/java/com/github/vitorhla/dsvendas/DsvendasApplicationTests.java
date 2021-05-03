7179281180f5f84d3dbad1ca2604a0cfa48b95db
vitorhla <vitorhenriquealt@gmail.com>
2021-05-03 08:54:24 -0300

    01-Project Created

---
+++ backend/src/test/java/com/github/vitorhla/dsvendas/DsvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.vitorhla.dsvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
