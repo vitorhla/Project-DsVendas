efc3a7157ddbda997d997dfad68a38b37942eb70
vitorhla <vitorhenriquealt@gmail.com>
2021-05-03 11:54:46 -0300

    05-Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+        <div className="container">
+          <p className="text-light">App desenvolvido por <a href="https://github.com/vitorhla" target="_blank" rel="noreferrer">Vitor H.L Andrade</a></p>
+          <p className="text-light"><small><strong>Tecnologias Spring React</strong><br/>
+            Acesse meu instagram pelo link: <a href="https://instagram.com/vitor.hla" target="_blank" rel="noreferrer">@vitorhla</a></small></p>
+        </div>
+      </footer>
+    );
+}
+
+export default Footer;
