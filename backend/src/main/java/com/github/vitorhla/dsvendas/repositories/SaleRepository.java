04abb8c8361ad86257a008fe583b30f7cb38e850
vitorhla <vitorhenriquealt@gmail.com>
2021-05-06 08:46:45 -0300

    10-Pagination

---
+++ backend/src/main/java/com/github/vitorhla/dsvendas/repositories/SaleRepository.java
@@ -0,0 +1,9 @@
+package com.github.vitorhla.dsvendas.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.github.vitorhla.dsvendas.entities.Sale;
+
+public interface SaleRepository extends JpaRepository <Sale,Long>{
+
+}
